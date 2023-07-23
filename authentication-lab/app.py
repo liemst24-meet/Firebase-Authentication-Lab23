b0588dbf80742b06bd73bd176d02da961fe9ddf6
liemst24-meet <liem.strykowski@gmail.com>
2023-07-23 18:27:42 +0300

    help me
    i want to go home plz

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,23 +2,65 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+
+config = {
+  "apiKey": "AIzaSyBoLHj9lGeYp7xt_3f7otRsE831g9hEpcA",
+  "authDomain": "help-441e0.firebaseapp.com",
+  "projectId": "help-441e0",
+  "storageBucket": "help-441e0.appspot.com",
+  "messagingSenderId": "376818407257",
+  "appId": "1:376818407257:web:efb817bd9381dfd9ca5e48",
+  "databaseURL":""
+}
+
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
+    error = ""
+    if request.method == 'POST':
+       email = request.form['email']
+       password = request.form['password']
+       try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+       except:
+           error = "Authentication failed"
     return render_template("signin.html")
+
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+        except:
+            error = "Authentication failed"
+
     return render_template("signup.html")
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
     return render_template("add_tweet.html")
+
+@app.route('/signout')
+def signout():
+    login_session['user'] = None
+    auth.current_user = None
+    return redirect(url_for('signin'))
 
 
 if __name__ == '__main__':
